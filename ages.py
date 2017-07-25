b34f5947bda1b65555a437c6e8e44f841165e12b
idane19-meet <idane19@meet.mit.edu>
2017-07-25 17:26:12 +0200

    Stopping here

---
+++ ages.py
@@ -0,0 +1,5 @@
+age1 = int(input('what is your age: '))
+
+age2 = int(input('what is your age other person: '))
+
+print('Did you know?!?the sum of your ages is ' + str(age1 + age2))
---
+++ inputoutput.py
@@ -0,0 +1,9 @@
+print('how old are you?')
+
+age = input()
+
+if int(age) > 50: 
+    print('your age is ' + age + ' your old man!')
+
+else:
+    print('your age is ' + age + ".")
